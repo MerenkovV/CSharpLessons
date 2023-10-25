8e3e6b28c0e6139cc0fb18fd2cdf3baa20a03195
Vlad <merenkovvladislav@mail.ru>
2023-10-25 15:36:42 +0700

    Start the project and study console methods

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace firstOfAll{
+    class Program{
+        static void Main(){
+            bool loiading = false;
+            int number = 100;
+            Console.WriteLine(number);
+            Console.WriteLine("Hey");
+            Console.WriteLine("Loading? - " + loiading);
+
+            int num_1, num_2;
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine('+');
+            num_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine('=');
+            Console.WriteLine(num_1+num_2);
+        }
+    }
+}
