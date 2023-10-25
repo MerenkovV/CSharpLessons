7ba428adf38b8bb5c4bff16da32494c30ae7f13c
Vlad <merenkovvladislav@mail.ru>
2023-10-25 22:31:00 +0700

    Study arrays

--- Program.cs
+++ Program.cs
@@ -1,20 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace firstOfAll{
     class Program{
         static void Main(){
-            bool loiading = false;
-            int number = 100;
-            Console.WriteLine(number);
-            Console.WriteLine("Hey");
-            Console.WriteLine("Loading? - " + loiading);
+            // short length = Convert.ToInt16(Console.ReadLine());
+            // string?[] names = new string[length];
+            // for(short index = 0; index < names.Length; index++){
+            //     names[index] = Console.ReadLine();
+            // }
 
-            int num_1, num_2;
-            num_1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine('+');
-            num_2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine('=');
-            Console.WriteLine(num_1+num_2);
+            // for(short index = 0; index < names.Length; index++){
+            //     Console.WriteLine(names[index] != null ? names[index] : "void");
+            // }
+
+            //foreach(string? name in names) Console.WriteLine(name != null ? name : "void");
+
+            //Динамические массивы (хранят любое количество элементов + имеют дополнительные методы):
+
+            List<short> numbers = new List<short> {
+                132, 13, 44, 16
+            };
+
+            numbers.Add(14);
+            numbers.Sort();
+            numbers.Reverse();
+            numbers.Remove(13);
+
+            foreach (short number in numbers) System.Console.WriteLine(number);
+
+
         }
     }
 }
